2df6e659aaec7ebe3fb8e0134b8e5afde3faaa87
DEKHTIAR Jonathan <contact@jonathandekhtiar.eu>
2016-10-14 16:52:25 +0200

    PostgreSQL Instead of SQLite

--- mysite/manage.py
+++ mysite/manage.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 import os
 import sys
 
+import dotenv
+
 if __name__ == "__main__":
+    
+    platforms = ["TRAVIS"]
+    if not any(x in os.environ for x in platforms):
+        dotenv.read_dotenv()
+        
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
