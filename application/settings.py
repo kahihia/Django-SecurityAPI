7cb6a07b22fff801a0c4a02f807c262da7d10ded
DEKHTIAR Jonathan <contact@jonathandekhtiar.eu>
2016-10-31 18:23:27 +0100

    Fixes in settings.py + updated requirements

--- application/settings.py
+++ application/settings.py
@@ -42,16 +42,6 @@ SECRET_KEY = assign_env_value('SECRET_KEY')
 DEBUG = assign_env_value('DEBUG')
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: change this before deploying to production!
-SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
 # Application definition
 
 INSTALLED_APPS = (
