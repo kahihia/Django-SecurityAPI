386a4b88938bb25404008d293251d23344ead19f
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2016-12-02 17:20:52 +0100

    Landing page suppression

--- landing_page/test_all.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-from application.settings import *
-
-sys.path.append(os.path.join(BASE_DIR, 'landing_page/tests'))
-
-
-from test_landing_page_admin import *
-from test_landing_page_views import *
