ea4fa3c91e3d1551ae324ca46d7d7ecfb011fa66
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-12-12 21:28:05 +0100

    Migration Script fixed

--- migrateDB_Win.bat
+++ migrateDB_Win.bat
@@ -1,6 +1,6 @@
 %~d1
 cd "%~p1"
 call venv\Scripts\activate.bat
-call python manage.py makemigrations landing_page api_v1_0
+call python manage.py makemigrations api_v1_0
 call python manage.py migrate
 PAUSE;
