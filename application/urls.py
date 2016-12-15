4aed34e281de7d31ca3f6836f434c3d153d560b2
jjerphan <julien.jerphanion@gmail.com>
2016-12-15 10:01:38 +0100

    Adding some endpoints views

--- application/urls.py
+++ application/urls.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals # for Python 3 with Python 2 retrocompatibility
 
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib.auth.models import User, Group
 from django.contrib import admin
@@ -10,6 +11,8 @@ admin.autodiscover()
 from django.contrib.auth import views as auth_views
 from rest_framework import permissions, routers, serializers, viewsets
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
+import oauth2_provider.views as oauth2_views
+from .views import ApiEndpoint
 
 # first we define the serializers
 class UserSerializer(serializers.ModelSerializer):
@@ -34,6 +37,30 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+# OAuth2 provider endpoints
+oauth2_endpoint_views = [
+    url(r'^authorize/$', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    url(r'^token/$', oauth2_views.TokenView.as_view(), name="token"),
+    url(r'^revoke-token/$', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        url(r'^applications/$', oauth2_views.ApplicationList.as_view(), name="list"),
+        url(r'^applications/register/$', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+        url(r'^applications/(?P<pk>\d+)/$', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+        url(r'^applications/(?P<pk>\d+)/delete/$', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+        url(r'^applications/(?P<pk>\d+)/update/$', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+    ]
+
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        url(r'^authorized-tokens/$', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+        url(r'^authorized-tokens/(?P<pk>\d+)/delete/$', oauth2_views.AuthorizedTokenDeleteView.as_view(),
+            name="authorized-token-delete"),
+    ]
+
 
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
@@ -47,6 +74,7 @@ router.register(r'groups', GroupViewSet)
 urlpatterns = [
 	url(r'^', include(router.urls)),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(r'^accounts/login/$', auth_views.login,{'template_name': 'registration/login.html'}),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', auth_views.login,{'template_name': 'registration/login.html'}),
+    url(r'^api/hello', ApiEndpoint.as_view()),
 ]
