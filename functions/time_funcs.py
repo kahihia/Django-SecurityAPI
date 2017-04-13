e197698f153336b3745d81209b15e99ea7b83321
jjerphan <julien.jerphanion@protonmail.com>
2017-04-13 21:19:29 +0200

    Front-End enhancements

--- functions/time_funcs.py
+++ functions/time_funcs.py
@@ -2,8 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals # for Python 3 with Python 2 retrocompatibility
+from django.shortcuts import render
+from django.conf import settings
 
 import datetime, time
 
 def get_timestamp():
     return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def customRender(request, template, dictionary={}):
+    dictionary.update({'post_count': Post.objects.count()})
+
+    if Option.objects.filter(parameter="display_user_count").exists():
+        dictionary.update({'display_user_count': Option.objects.get(parameter="display_user_count").get_bool_value()})
+    else:
+        dictionary.update({'display_user_count':False})
+
+    return render(request, template, dictionary)
